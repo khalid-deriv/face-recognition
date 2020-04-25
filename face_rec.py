6fa5ae2f3d74ad9585162a142ab08541cd619556
khalidxx <fxtryio@gmail.com>
2020-04-25 16:20:02 +0800

    indentation

--- face_rec.py
+++ face_rec.py
@@ -44,7 +44,7 @@ while True:
 			results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)[0]
 		except:
 			results = False
-			
+
 		if results == True:
 			print("It's a picture of me!")
 			text = 'Khalid'
@@ -61,34 +61,3 @@ while True:
 
 	if key == 27:
 		exit()
-
-
-
-				
-
-
-
-
-
-
-
-
-
-# # picture_of_me = face_recognition.load_image_file("khalid_ID.jpg")
-# picture_of_me = cv2.imread("khalid_ID.jpg")
-# picture_of_me = cv2.cvtColor(picture_of_me, cv2.COLOR_BGR2RGB)
-# my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
-
-# # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
-
-# unknown_picture = face_recognition.load_image_file("khalid_2.jpg")
-# unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-
-# # Now we can see the two face encodings are of the same person with `compare_faces`!
-# start = time.time()
-# results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-# end = time.time()
-# if results[0] == True:
-#		 print("It's a picture of me!", str(end-start), "seconds")
-# else:
-#		 print("It's not a picture of me!", str(end-start), "seconds")
